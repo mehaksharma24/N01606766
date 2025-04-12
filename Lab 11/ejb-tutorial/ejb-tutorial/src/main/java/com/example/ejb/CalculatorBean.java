ef9990e24a1bc6e59cda975a2e81905812a11a60
mehaksharma24 <145503146+mehaksharma24@users.noreply.github.com>
2025-04-12 19:29:41 -0400

    lab11

---
+++ Lab 11/ejb-tutorial/ejb-tutorial/src/main/java/com/example/ejb/CalculatorBean.java
@@ -0,0 +1,15 @@
+package com.example.ejb;
+
+import jakarta.ejb.Stateless;
+
+@Stateless
+public class CalculatorBean {
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public int substract(int a, int b) {
+        return a - b;
+    }
+    public int multiply(int a, int b) { return a * b; }
+    public int divide(int a, int b) { return a / b; }
+}
---
+++ Lab 11/ejb-tutorial/ejb-tutorial/src/main/java/com/example/ejbtutorial/HelloServlet.java
@@ -0,0 +1,29 @@
+package com.example.ejbtutorial;
+
+import com.example.ejb.CalculatorBean;
+
+import jakarta.ejb.EJB;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    @EJB
+    private CalculatorBean calculatorBean;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        int sum = calculatorBean.add(10, 5);
+        response.getWriter().println("10 + 5 = " + sum);
+        int difference = calculatorBean.substract(10, 5);
+        response.getWriter().println("10 - 5 = " + difference);
+        int product = calculatorBean.multiply(10, 5);
+        response.getWriter().println("10 * 5 = " + product);
+        double result = calculatorBean.divide(10, 5);
+        response.getWriter().println("10 / 5 = " + result);
+    }
+}
